6c3bfb163c4de43c249b0e21996f9b1b6e72a6a2
araj-abood <alaraj.abdullah02@gmail.com>
2025-09-27 16:43:38 +0300

    Add verbose

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 import sys
 
 def main():
@@ -13,18 +14,35 @@ def main():
 
     user_prompt = " ".join(arguments[1:])
 
+    if "--verbose" in user_prompt:
+        user_prompt = user_prompt.replace("--verbose", "")
+
     if not user_prompt:
-        print("No prompt provided")
+        print("No prompt provided")        
+        print("AI Code Assistant")
+        print('\nUsage: python main.py "your prompt here"')
+        print('Example: python main.py "How do I build a calculator app?"')
         exit(1)
 
-    response = clinet.models.generate_content(model="gemini-2.0-flash-001", contents=user_prompt)
+    is_verbose = "--verbose" in arguments
+    
+    messages = [types.Content(role="user", parts=[types.Part(text=user_prompt)])]
+
+
+    response = clinet.models.generate_content(
+        model="gemini-2.0-flash-001", 
+        contents=messages
+    
+    )
 
     response_text = response.text
-
+    if is_verbose:
+        print(f"User prompt: {user_prompt}")
+        meta_data = response.usage_metadata
+        print(f"Prompt tokens: {meta_data.prompt_token_count}")
+        print(f"Response tokens: {meta_data.candidates_token_count}")
+    print("Response:")
     print(response_text)
-    meta_data = response.usage_metadata
-    print(f"Prompt tokens: {meta_data.prompt_token_count}")
-    print(f"Response tokens: {meta_data.candidates_token_count}")
 
 if __name__ == "__main__":
     main()
