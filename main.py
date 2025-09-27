b1583cb2e3be6172d897bd0dcc1e884962dc44ae
araj-abood <alaraj.abdullah02@gmail.com>
2025-09-27 16:33:33 +0300

    Add user rompt for argv:

--- main.py
+++ main.py
@@ -2,15 +2,29 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+import sys
 
 def main():
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     clinet = genai.Client(api_key=api_key)
 
-    response = clinet.models.generate_content(model="gemini-2.0-flash-001", contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
+    arguments = sys.argv
 
-    print(response.text)
+    user_prompt = " ".join(arguments[1:])
+
+    if not user_prompt:
+        print("No prompt provided")
+        exit(1)
+
+    response = clinet.models.generate_content(model="gemini-2.0-flash-001", contents=user_prompt)
+
+    response_text = response.text
+
+    print(response_text)
+    meta_data = response.usage_metadata
+    print(f"Prompt tokens: {meta_data.prompt_token_count}")
+    print(f"Response tokens: {meta_data.candidates_token_count}")
 
 if __name__ == "__main__":
     main()
