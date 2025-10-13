c9234054f5faa963cf2485e56980b5683f3a6c88
araj-abood <alaraj.abdullah02@gmail.com>
2025-10-14 00:17:45 +0300

    create the write_file_schema for genai

--- functions/write_file.py
+++ functions/write_file.py
@@ -1,4 +1,5 @@
 import os
+from google.genai import types
 def write_file(working_directory, file_path, content):
     working_directory_abs = os.path.abspath(working_directory)
     file_path_abs = os.path.abspath(os.path.join(working_directory_abs, file_path))
@@ -10,3 +11,31 @@ def write_file(working_directory, file_path, content):
     with open(file_path_abs, "w") as f:
         f.write(content)
         return f'Successfully wrote to "{file_path}" ({len(content)} characters written)'
+
+
+
+schema_write_file = types.FunctionDeclaration(
+    name="write_file",
+        description=(
+            "Writes text content to a file. "
+            "Use ONLY when the user explicitly requests to 'save', 'create', or 'update' a file. "
+            "NEVER use this to list, explore, or inspect directories — use 'get_files_info' instead."
+        ),
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "working_directory": types.Schema(
+                type=types.Type.STRING,
+                description="The base folder where the file is located."
+            ),
+            "file_path": types.Schema(
+                type=types.Type.STRING,
+                description="The relative or absolute path of the file to be written."
+            ),
+            "content": types.Schema(
+                type=types.Type.STRING,
+                description="The text content to write into the target file."
+            ),
+        }
+    )
+)
